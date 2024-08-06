09a954ef4e29717a02d82c4bd31ff2187aefaa4e
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-08-06 20:04:18 +0300

    finish functionArrow tasks

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+// let numbers = [2,4,5,8,9];
+nums = [2,3,4];
+const greet = (name) => `Hello ${name}!` 
+
+const sum = (num1, num2) => num1 + num2; 
+const square = (number) => number * number;
+const squareArray = nums.map((number) => {
+  return number * number;
+});
+
+
+console.log(squareArray)
+
+
